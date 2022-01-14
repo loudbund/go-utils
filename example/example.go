12111e6b8d4777382e08d24af655006d5fcede27
DESKTOP-A6285B0\zhoujian <24086215@qq.com>
2022-01-14 10:11:10 +0800

    清理掉没有使用的依赖

--- example/example.go
+++ example/example.go
@@ -2,12 +2,6 @@ package main
 
 import (
 	"fmt"
-	_ "github.com/loudbund/go-json/json_v1"
-	_ "github.com/loudbund/go-mysql/mysql_v1"
-	_ "github.com/loudbund/go-pool/pool_v1"
-	_ "github.com/loudbund/go-progress/progress_v1"
-	_ "github.com/loudbund/go-request/request_v1"
-	_ "github.com/loudbund/go-socket/socket_v1"
 	"github.com/loudbund/go-utils/utils_v1"
 	_ "github.com/loudbund/go-utils/utils_v1"
 )
